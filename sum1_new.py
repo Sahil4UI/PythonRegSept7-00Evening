676e3f23ee87d7e541d8e525323e317a27aac540
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-09-23 19:53:07 +0530

    Add files via upload

---
+++ sum1_new.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter number 1:"))
+print(type(num1))
+num2 = int(input("Enter number 2:"))
+print(type(num2))
+result = num1+num2
+#the sum of 3 and 5 is 8
+#print("The sum of ",num1,"and",num2,"is",result)
+#print("the sum of {} and {} is {}".format(num1,num2,result))
+print(f"the sum of {num1} and {num2} is {result}")
