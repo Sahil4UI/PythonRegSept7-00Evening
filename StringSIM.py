39a53086114e0d41ca7a1c2ae929c3a1a04f4cdb
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2020-09-24 20:04:34 +0530

    Add files via upload

---
+++ StringSIM.py
@@ -0,0 +1,7 @@
+text1 = "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level built in data structures, combined with dynamic typing and dynamic binding, make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language to connect existing components together. Python's simple, easy to learn syntax emphasizes readability and therefore reduces the cost of program maintenance. Python supports modules and packages, which encourages program modularity and code reuse. The Python interpreter and the extensive standard library are available in source or binary form without charge for all major platforms, and can be freely distributed."
+text2 = 'Python is a popular general-purpose programming language that can be used for a wide variety of applications. It includes high-level data structures, dynamic typing, dynamic binding, and many more features that make it as useful for complex application development as it is for scripting or "glue code" that connects components together. It can also be extended to make system calls to almost all operating systems and to run code written in C or C++. Due to its ubiquity and ability to run on nearly every system architecture, Python is a universal language found in a variety of different applications.'
+
+
+#tokenization
+token1 = set(text1.split())
+token2 = set(text2.split())
